c699edf0dbfa6c3430aa6c5d34f9756f83811b8c
Nguyễn Minh Chí <70262577+minhchi1509@users.noreply.github.com>
2022-02-09 14:05:08 +0700

    Create CHÊNH LỆCH NHỎ NHẤT.cpp

---
+++ CHÊNH LỆCH NHỎ NHẤT.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+void Solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a)
+        cin >> x;
+    sort(a.begin(), a.end());
+    int ans = INT_MAX;
+    for (int i = 1; i < n; i++)
+        ans = min(ans, abs(a[i] - a[i - 1]));
+    cout << ans << "\n";
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Solve();
+    }
+    return 0;
+}
