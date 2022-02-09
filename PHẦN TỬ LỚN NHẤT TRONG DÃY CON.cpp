33272137e2109028fc64f3009987878d0db72b7e
Nguyễn Minh Chí <70262577+minhchi1509@users.noreply.github.com>
2022-02-09 12:43:40 +0700

    Create PHẦN TỬ LỚN NHẤT TRONG DÃY CON.cpp

---
+++ PHẦN TỬ LỚN NHẤT TRONG DÃY CON.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+void Solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (auto &x : a)
+        cin >> x;
+    for (int i = 0; i <= n - k; i++)
+        cout << *max_element(a.begin() + i, a.begin() + i + k) << " ";
+    cout << "\n";
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Solve();
+    }
+    return 0;
+}
