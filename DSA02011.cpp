d8809d6e3db607c05059f0868e8517f652dd8acd
Nguyễn Minh Chí <70262577+minhchi1509@users.noreply.github.com>
2022-02-12 09:36:22 +0700

    Create DSA02011.cpp

---
+++ DSA02011.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long;
+struct Data
+{
+    vector<int> v;
+    int sum;
+};
+void Solve()
+{
+    int n, S;
+    cin >> n >> S;
+    vector<int> money(n);
+    for (auto &x : money)
+        cin >> x;
+    sort(money.begin(), money.end());
+    unordered_set<int> diff;
+    queue<Data> q;
+    q.push({money, 0});
+    while (!q.empty())
+    {
+        Data tmp = q.front();
+        q.pop();
+        if (tmp.sum == S)
+        {
+            cout << (n - tmp.v.size()) << "\n";
+            return;
+        }
+        vector<int> vv = tmp.v;
+        for (int i = 0; i < (int)vv.size(); i++)
+        {
+            if (diff.find(tmp.sum + vv[i]) == diff.end() && tmp.sum + vv[i] <= S)
+            {
+                vector<int> arr = vv;
+                arr.erase(arr.begin() + i);
+                q.push({arr, tmp.sum + vv[i]});
+                diff.insert(tmp.sum + vv[i]);
+            }
+        }
+    }
+    cout << -1 << "\n";
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Solve();
+    }
+    return 0;
+}
