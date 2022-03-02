6edda4ef042278d5d0a244b1ff13628b18be6a8d
Nguyễn Minh Chí <70262577+minhchi1509@users.noreply.github.com>
2022-03-02 20:27:39 +0700

    Create DSA05005.cpp

---
+++ DSA05005.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+void Solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), dp(n, 1);
+    for (auto &x : a)
+        cin >> x;
+    dp[0] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (a[i] >= a[j])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+    cout << n - *max_element(dp.begin(), dp.end()) << "\n";
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Solve();
+    }
+    return 0;
+}
