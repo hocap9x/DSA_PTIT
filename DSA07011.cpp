35f705949177f9fa503e602dd2ab8811ab8e0a33
Nguyễn Minh Chí <70262577+minhchi1509@users.noreply.github.com>
2022-03-03 21:01:59 +0700

    Create DSA07011.cpp

---
+++ DSA07011.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+void Solve()
+{
+    string s;
+    cin >> s;
+    stack<string> stk;
+    for (int i = 0; i < (int)s.size(); i++)
+    {
+        if (isalpha(s[i]))
+        {
+            string tmp(1, s[i]);
+            stk.push(tmp);
+        }
+        else
+        {
+            string a = stk.top();
+            stk.pop();
+            string b = stk.top();
+            stk.pop();
+            stk.push(s[i] + b + a);
+        }
+    }
+    cout << stk.top() << "\n";
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Solve();
+    }
+    return 0;
+}
