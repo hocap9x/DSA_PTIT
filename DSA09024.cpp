5cf8a3ee1b5dbaf15a069c5bc991b5632b27ea6e
Nguyễn Minh Chí <70262577+minhchi1509@users.noreply.github.com>
2022-03-05 22:53:45 +0700

    Create DSA09024.cpp

---
+++ DSA09024.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+const int MAX = 1e3 + 5;
+vector<int> adj[MAX];
+bool vis[MAX];
+void BFS(int s)
+{
+    queue<int> q;
+    q.push(s);
+    vis[s] = true;
+    while (!q.empty())
+    {
+        int temp = q.front();
+        cout << temp << " ";
+        q.pop();
+        for (int i = 0; i < adj[temp].size(); i++)
+        {
+            if (!vis[adj[temp][i]])
+            {
+                vis[adj[temp][i]] = true;
+                q.push(adj[temp][i]);
+            }
+        }
+    }
+}
+void Solve()
+{
+    for (int i = 0; i < MAX; i++)
+        adj[i].clear();
+    memset(vis, false, sizeof(vis));
+    int V, E, s;
+    cin >> V >> E >> s;
+    for (int i = 0; i < E; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+    BFS(s);
+    cout << "\n";
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Solve();
+    }
+    return 0;
+}
