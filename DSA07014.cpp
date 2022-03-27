3a8d809a6c0d275bf9cdc91eec412236b1821e2d
Nguyễn Minh Chí <70262577+minhchi1509@users.noreply.github.com>
2022-03-27 16:31:46 +0700

    Create DSA07014.cpp

---
+++ DSA07014.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+int Calc(int a, int b, char x)
+{
+    if (x == '+')
+        return a + b;
+    else if (x == '-')
+        return a - b;
+    else if (x == '*')
+        return a * b;
+    return a / b;
+}
+void Solve()
+{
+    string s;
+    cin >> s;
+    stack<int> stk;
+    for (int i = s.size() - 1; i >= 0; i--)
+    {
+        if (isdigit(s[i]))
+            stk.push(s[i] - '0');
+        else
+        {
+            int m1 = stk.top();
+            stk.pop();
+            int m2 = stk.top();
+            stk.pop();
+            stk.push(Calc(m1, m2, s[i]));
+        }
+    }
+    cout << stk.top() << "\n";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        Solve();
+    }
+    return 0;
+}
